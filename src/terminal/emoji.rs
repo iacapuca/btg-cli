c1078d8b4fbc13595e11403b346edd4fef0b28ae
Pedro Paes <plpbs@poli.br>
2022-06-28 15:03:01 -0300

    Added Emoji file

--- src/terminal/emoji.rs
+++ src/terminal/emoji.rs
@@ -1,16 +1,3 @@
-//! Emoji constants used by `wrangler`.
-//!
-//! For the unfamiliar:
-//!
-//! > Emoji are ideograms and smileys used in electronic messages and web
-//! > pages. Emoji exist in various genres, including facial expressions, common
-//! > objects, places and types of weather, and animals. They are much like
-//! > emoticons, but emoji are actual pictures instead of typographics.
-//!
-//! -- https://en.wikipedia.org/wiki/Emoji
-
-#![allow(missing_docs)]
-
 use console::Emoji;
 
 pub static DOWN: Emoji = Emoji("⬇️ ", "");
